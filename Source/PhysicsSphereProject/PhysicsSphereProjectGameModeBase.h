3c9c8340eeecc24fd28582ac5e7f49b3dc6f56ed
Matthew Robinson <73129795+Matthew-t-robinson@users.noreply.github.com>
2023-09-26 15:55:37 +0100

    Initial Commit

---
+++ Source/PhysicsSphereProject/PhysicsSphereProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PhysicsSphereProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PHYSICSSPHEREPROJECT_API APhysicsSphereProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
